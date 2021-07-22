6fafdc70f12834c74401e7a50470488da99cda99
korir Cornelius kiprono <ceekayconelius@gmail.com>
2021-07-22 15:22:25 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({ title,onAdd,showAdd }) => {
+// passing functions as props
+
+    return (
+        <header className='header'>
+            <h1> {title} </h1>
+            <Button 
+            onClick={onAdd} 
+            color={showAdd?'green':'red'} 
+            text={showAdd ? 'New Task':'Close'}/>
+        </header>
+)
+}
+
+//example of default props
+//
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+// you can be more specific with your propr types and give them default types.
+
+Header.propTypes = {
+    //generate error in the dev console when the title is unavailable and diffrent data type.
+    title: PropTypes.string.isRequired,
+}
+
+// example of inline styling e.g <h1 style = {headingStyle}> </h1> 
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// } 
+
+export default Header
